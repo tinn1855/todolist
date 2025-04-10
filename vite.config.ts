323d5aa5b08f9993a9f3f373cf8d1ca78caf90e6
tinnguyen <tinn1855@gmail.com>
2025-04-10 11:48:11 +0700

    init repo

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
+import path from 'path';
 
 export default defineConfig(() => ({
   root: __dirname,
@@ -37,6 +38,11 @@ export default defineConfig(() => ({
     coverage: {
       reportsDirectory: './coverage/todo-list',
       provider: 'v8' as const,
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
   },
 }));
